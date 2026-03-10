22bee98ce647d350308b050fc11ea0e6ced1cc50
Sakeel1406 <144681263+Sakeel1406@users.noreply.github.com>
2026-03-10 09:17:41 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './App.css'
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter,Routes} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/portfolio-sakeel">
+      <Routes>
       <App />
+        </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
