15bba30ad991bbedc0b59f8138f49a65568dd049
Sakeel1406 <144681263+Sakeel1406@users.noreply.github.com>
2026-03-10 09:28:32 +0530

    Refactor assets.js to clean up commented code

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,17 +3,12 @@ import background from './background.jpg';
 
 import my_Photo from './my_Photo.png';
 
-// export const photo= {
-//     p1_img1,
-//     p1_img2,
-//     p1_img3,
-//     p1_img4,
-//     p1_img5
-// }
+
 
 export const assets = {
     
     background,
     my_Photo,
    
+
 }
