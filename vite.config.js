9c83734aa35d1716ca4b14e052b834838ace6a33
Sakeel1406 <144681263+Sakeel1406@users.noreply.github.com>
2026-03-10 09:03:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  // This says: if we're on GitHub Pages, use the subfolder, otherwise use the root.
+  base: process.env.NODE_ENV === 'production' ? '/portfolio-sakeel/' : '/',
   plugins: [react()],
-  base: "/portfolio-sakeel/"
 });
