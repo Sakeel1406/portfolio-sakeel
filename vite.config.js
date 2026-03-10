0154b255eb72f569a7daed03e5026a2858fb0594
Sakeel1406 <144681263+Sakeel1406@users.noreply.github.com>
2026-03-10 09:13:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // This tells Vite: use a subfolder for GitHub, but root for Vercel
-  base: process.env.VERCEL ? '/' : '/portfolio-sakeel/',
+  base: '/', // Strictly use root for Vercel
 })
