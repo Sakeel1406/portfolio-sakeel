820b7b61fac31dfb9eef93521632031d598f1ae1
Sakeel1406 <144681263+Sakeel1406@users.noreply.github.com>
2026-03-10 07:44:24 +0530

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,19 @@
+
+import background from './background.jpg';
+
+import my_Photo from './my_Photo.png';
+
+// export const photo= {
+//     p1_img1,
+//     p1_img2,
+//     p1_img3,
+//     p1_img4,
+//     p1_img5
+// }
+
+export const assets = {
+    
+    background,
+    my_Photo,
+   
+}
